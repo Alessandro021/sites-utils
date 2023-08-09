8c07e90fe9aa0b924c71903cbd1ecb28264c6a58
alessandro <alessandrodamata6@gmail.com>
2023-08-09 17:28:35 -0300

    Algumas correçoes

--- src/components/WebViewBrowser.js
+++ src/components/WebViewBrowser.js
@@ -1,19 +1,18 @@
-import * as WebBrowser from 'expo-web-browser';
+import {mayInitWithUrlAsync, openBrowserAsync} from 'expo-web-browser';
 
 export default async function WebViewBrowser(url) {
-
-  const browserResult = await WebBrowser.mayInitWithUrlAsync(url);
-
-  if (browserResult) {
-      await WebBrowser.openBrowserAsync(url, {
-        showTitle: true, // exibe ou oculta o título da página (opcional)
-        enableBarCollapsing: true, // Oculta a barra de endereço e de ferramentas do navegador ao rolar para baixo
-        toolbarColor: '#3498db', // Define a cor da barra de ferramentas do navegador
-        browserPackage: browserResult.servicePackage,
-        cookieEnabled: true, // Define a política de cookies para permitir ou bloquear e armazenamento de cookies na página carregada
-        toolbar: true // oculta a barra de ferramentas e endereço do navegador
-      });
-    } else {
-        await WebBrowser.openBrowserAsync(url);
-    }
+  await mayInitWithUrlAsync(url)
+    .then(async browserResult => await openBrowserAsync(url, {
+      browserPackage: browserResult.servicePackage,
+      showTitle: true, // exibe ou oculta o título da página (opcional)
+      enableBarCollapsing: true, // Oculta a barra de endereço e de ferramentas do navegador ao rolar para baixo
+      toolbarColor: '#3498db', // Define a cor da barra de ferramentas do navegador
+      cookieEnabled: true, // Define a política de cookies para permitir ou bloquear e armazenamento de cookies na página carregada
+      toolbar: true, // oculta a barra de ferramentas e endereço do navegador
+      showInRecents: false,
+      createTask: false,
+     })
+      .catch(async () => await WebBrowser.openBrowserAsync(url))
+    )
+    .catch(() => { return })
 }
